bd0d64b554af3dc22c79b5cdc7812daccf462a80
yasminebcf <yasminebcf21@gmail.com>
2026-02-19 15:26:06 +0000

    Upgrade to Q-Learning Agent

--- agent.py
+++ agent.py
@@ -1,10 +1,42 @@
 # agent.py
 import random
+import pickle
 
-class RandomAgent:
-    def __init__(self, player=-1):
-        self.player = player  # -1 = AI
+class QLearningAgent:
+    def __init__(self, player=-1, alpha=0.5, gamma=0.9, epsilon=0.2):
+        self.player = player
+        self.q_table = {}  # state: {action: value}
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
 
-    def choose_move(self, available_moves):
-        # Just pick a random empty spot
-        return random.choice(available_moves)
+    def get_q(self, state, action):
+        if state not in self.q_table:
+            self.q_table[state] = {a:0 for a in range(9)}
+        return self.q_table[state][action]
+
+    def choose_move(self, state, available_moves):
+        # Exploration
+        if random.random() < self.epsilon:
+            return random.choice(available_moves)
+
+        # Exploitation
+        q_values = {a:self.get_q(state,a) for a in available_moves}
+        max_q = max(q_values.values())
+        best_moves = [a for a,v in q_values.items() if v==max_q]
+        return random.choice(best_moves)
+
+    def update_q(self, state, action, reward, next_state, next_available):
+        max_future = 0
+        if next_available:
+            max_future = max([self.get_q(next_state,a) for a in next_available])
+        old_q = self.get_q(state,action)
+        self.q_table[state][action] = old_q + self.alpha * (reward + self.gamma*max_future - old_q)
+
+    def save_q(self, filename="q_table.pkl"):
+        with open(filename,"wb") as f:
+            pickle.dump(self.q_table,f)
+
+    def load_q(self, filename="q_table.pkl"):
+        with open(filename,"rb") as f:
+            self.q_table = pickle.load(f)
